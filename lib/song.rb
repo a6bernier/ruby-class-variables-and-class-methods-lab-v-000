2eee7dbfece171db445863aedbbeb56b3f437150
Andrew Bernier <a6bernier@gmail.com>
2018-10-15 16:31:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,25 +27,15 @@ class Song
     @@artists.uniq
   end
 
-
+#returns hash in which the keys are the names of each genre
   #each genre name key should point to a value that is the number of songs that have that genre
-
+#need to iterate over @@genres and populate a hash with key/value pairs
   #need to check if hash already contains a key of a particular genre
       #if so increment the value of that key by one
       #otherwise create a new key/value pair
 
   def self.genre_count
-    genre_hash = {}
-    genre_count = 0
-    @@genres.each do |genre| #need to iterate over @@genres and populate a hash with key/value pairs
-      if genre == genre
-        genre_count += 1
-        genre_hash["genre"] << genre_count
-      else
-        genre_count = 1
-      end
-    end
-    genre_hash #returns hash in which the keys are the names of each genre
+
   end
 
 end
