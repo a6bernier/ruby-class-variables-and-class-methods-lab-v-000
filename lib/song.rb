8c19127a0560593eaf06f0fc73d0255793b069a5
Andrew Bernier <a6bernier@gmail.com>
2018-10-15 16:48:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
 
   def self.genre_count
     genre_hash = {}
-    @@genres.each do |genre|
+    @@genres.count do |genre|
       if genre_hash[genre]
         genre_hash[genre]
       else
